9f10b6b33e6bdd3a89db95049b5b665005a3c524
kunnikarB <kunnikarboon@gmail.com>
2025-08-25 07:47:26 +0200

    add alert to show pop-up windows

--- Introduction/script.js
+++ Introduction/script.js
@@ -5,3 +5,7 @@ const lovesCoding = true; // boolean
 
 // Print them to the console
 console.log(`Hi, my name is ${name}, I'm ${age} year old, and It's ${lovesCoding} that I love coding!`);
+
+alert(
+  `Hi, my name is ${name}, I'm ${age} year old, and It's ${lovesCoding} that I love coding!`
+);
