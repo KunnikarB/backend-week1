62bf5bb56be563a69effca61325b93aa2110b6b4
kunnikarB <kunnikarboon@gmail.com>
2025-08-25 15:43:15 +0200

    Advanced practice

--- Introduction/homework.js
+++ Introduction/homework.js
@@ -3,11 +3,9 @@ const firstName = 'Kunnikar'; // string
 let favoriteNumber = 9; // number
 const likesCoding = true; // boolean
 
-
 //Add two more variables
 const birthYear = 1977; // number
 const currentYear = 2025; // number
-
 
 // Calculate age
 const age = currentYear - birthYear;
@@ -21,6 +19,22 @@ console.log(
 console.log(`I am ${age} years old.`);
 
 
+/**********************************************************/
 
+// Make an array of 3 foods
+const favoriteFoods = ['Peking duck', 'Sushi', 'Pad Thai'];
 
+// Create an object called "student"
+const student = {
+  name: firstName,
+  favoriteNumber: favoriteNumber,
+  likesCoding: likesCoding,
+  foods: favoriteFoods
+};
+
+// Print the whole object
+console.log(student);
+
+// Print one of the properties
+console.log(`One of my favorite foods is ${student.foods[0]}.`);
 
