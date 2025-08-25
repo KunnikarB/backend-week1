9471857e9d4915748c991758a50cb4e60e0e1992
kunnikarB <kunnikarboon@gmail.com>
2025-08-25 15:22:46 +0200

    Declare variables

---
+++ Introduction/homework.js
@@ -0,0 +1,11 @@
+// Declare 3 variables
+const firstName = 'Kunnikar'; // string
+let favoriteNumber = 9; // number
+const likesCoding = true; // boolean
+
+// Print them to the console
+console.log(
+  `Hi, my name is ${firstName}, my favorite number is ${favoriteNumber} , and It's ${likesCoding} that I like coding!`
+);
+
+
