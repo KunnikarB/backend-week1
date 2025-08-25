09a514c723212dbb9450c0ff88530bb71b437b95
kunnikarB <kunnikarboon@gmail.com>
2025-08-25 15:30:30 +0200

    Extra practice

--- Introduction/homework.js
+++ Introduction/homework.js
@@ -3,9 +3,24 @@ const firstName = 'Kunnikar'; // string
 let favoriteNumber = 9; // number
 const likesCoding = true; // boolean
 
+
+//Add two more variables
+const birthYear = 1977; // number
+const currentYear = 2025; // number
+
+
+// Calculate age
+const age = currentYear - birthYear;
+
 // Print them to the console
 console.log(
   `Hi, my name is ${firstName}, my favorite number is ${favoriteNumber} , and It's ${likesCoding} that I like coding!`
 );
+
+// Print another sentence with age
+console.log(`I am ${age} years old.`);
+
+
+
 
 
