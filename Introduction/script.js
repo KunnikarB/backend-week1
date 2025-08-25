8aefecff0873639671b4010ab5212f527f0531b5
kunnikarB <kunnikarboon@gmail.com>
2025-08-25 07:31:50 +0200

    first commit

---
+++ Introduction/script.js
@@ -0,0 +1,7 @@
+// Declare 3 variables
+const name = 'Kunnikar'; // string
+let age = 48; // number
+const lovesCoding = true; // boolean
+
+// Print them to the console
+console.log(`Hi, my name is ${name}, I'm ${age} year old, and It's ${lovesCoding} that I love coding!`);
